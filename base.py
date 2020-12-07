4273155d18e3c2ac0cdc6346166830b1d80aaa9d
Nick Seidenman <nick.seidenman@opticalsuperstore.com.au>
2020-12-07 23:08:37 +1100

    Moved phi & theta methods into Apex class.  Makes more sense there.

--- base.py
+++ base.py
@@ -1,40 +1,107 @@
 
 from math import pi, sin, cos, atan, sqrt
 
+tau = (1 + sqrt(5.0)) / 2.0  # a.k.a. phi
+
+
+class Apex(object):
+    """
+    Apices decribe the intersections of lines (either perimeter or breakdown)
+    within a *Geodesic*'s face triangle.
+
+    There are two coordinate systems and which one is used depends on the
+    aspect of the given apex being used.
+
+        - x, y, z coordinates correspond to the perimeter of the triangle,
+          with the tics being the terminal ends of breakdown lines.
+        - l, r coordinates correspond only to the [l]eft and [r] sides of
+          the triangle.
+
+    There is a simple mapping from x, y, z to l, r, but no inverse map.
+    """
+    def __init__(self, x: int, y: int, z: int):
+        self._x = x
+        self._y = y
+        self._z = z
+
+    def phi(self):
+        """Eq. 12.1 - p 74"""
+        if self._y == 0:
+            return pi / 2
+        return atan(self._x / self._y)
+
+    def theta(self):
+        """Eq 12.2 - p 74"""
+        if self._z == 0:
+            return pi / 2
+        return atan(sqrt(self._x**2 + self._y**2) / self._z)
+        
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def z(self):
+        return self._z
+
+    @property
+    def l(self):
+        return self._y
+
+    @property
+    def r(self):
+        return self._x + self._y
+
+    @property
+    def xyz(self):
+        return [self._x, self._y, self._z]
+
+    @property
+    def lr(self):
+        return [self._l, self._r]
+
+    def __repr__(self):
+        return str([self._x, self._y, self._z])
+
+
 class Geodesic(object):
     '''
     Base class for calculating geodesic chord factors and angles.
     '''
-    
-    def rho(self, v : int = 1):
+
+    def rho(self, v: int=1):
         '''
         Build three arrays representing a Class I, method 1 breakdown
         of a geodesic triangle face. These will be used later to
         construct the (x, y, z) tuples that will become input to the
         function(s) that calculate the spherical coordinates and ultimately
         the chord factors for the given geodesic."
-            
+
         [ref: Geodesic Mathematics, ch 12]
         '''
-        
+
         # Sanity check:
         if v < 1:
             raise ValueError("v must be positive integer")
         if v > 8:
             raise ValueError("v > 8?  Seriously???")
-                    
+
         result = list()
-            
+
         vp1 = v + 1   # Used frequently, so DRY.
         bseq = list(range(vp1))
         brev = bseq[::-1]
-        
+
         # rho_0
         rho_0 = list()
         for i in range(vp1):
             rho_0.extend(bseq[i:i+1]*(vp1-i))
         result.append(rho_0)
-            
+
         # rho_1
         rho_1 = list()
         for i in range(vp1):
@@ -46,88 +113,73 @@ class Geodesic(object):
         for i in range(vp1, -1, -1):
             rho_2.extend(bseq[:i])
         result.append(rho_2)
-      
+
         return result
 
-    def points(self, v: int = 1):
-        rho_set = self.rho(v)
-        result = [[rho_set[j][i] for j in range(3)] for i in range(len(rho_set[0]))]
-        return result
-        
-    def dist(self, p1, p2):
+    def _cvt(self, x, y, z, v):
         """
-        Calculate the distance between the two points, 
+        1v tetrahedron apices lie on the axes, but the two "lower" icosahedron
+        apices do not and thus their coordinates need to be adjusted before
+        passing them to the Apex constructor.
+        
+        The default conversion is simply an identity.
+        """
+        return x, y, z
+        
+    def apices(self, v: int=1):
+        if v < 1:
+            raise ValueError('v must be a positive integer')
+        rho_set = self.rho(v)
+        result = [Apex(*(self._cvt(rho_set[0][i], rho_set[1][i], rho_set[2][i], v)))
+                  for i in range(len(rho_set[0]))]
+        return result
+
+    def dist(self, a1, a2):  # r: float = 1
+        """
+        Calculate the distance between the two apices, a1 and a2,
         given their **spherical** coordinates (phi,theta).
         Radius (r) is assumed to be 1.
-        
+
         Eq. 9.2 - p 60 (sphere w/ r=1)
         """
-        return sqrt(2 - 2 * cos(p1[1]) * cos(p2[1]) + \
-                    cos(p1[0] - p2[0]) * sin(p1[1]) * sin(p2[1]))
-        
-                                                                   
-class GeodesicAngleMixin(object):
-    """
-    These are the angle calculations for TetraGeodesic.  They
-    can also be used for IcosaGeodesic with the angle correction functions.
-    """
-        
-class TetraGeodesic(Geodesic, GeodesicAngleMixin):
+        return sqrt(2 - (2 * cos(a1.theta()) * cos(a2.theta()) +
+                    cos(a1.phi() - a2.phi()) * sin(a1.theta()) * sin(a2.theta())))
+
+
+
+class TetraGeodesic(Geodesic):
     """
     Tetrahedron Geodesic Base
     -------------------------
     Because the tetrahedron is so spacially ineffecient, it is unlikely
     we'll ever use this class.  It is included only for completeness.
     """
-    
-    def phi(self, x: float, y: float, z: float):
-        """Eq. 12.1 - p 74"""
-        if y == 0:
-            return pi / 2
-        return atan(x / y)
+    def __init__(self, *args, **kwargs):
+        raise TypeError("Unimplemented!")
 
-    def theta(self, x: float, y: float, z: float):
-        """Eq 12.2 - p 74"""
-        if z == 0:
-            return pi / 2
-        return atan(sqrt(x**2 + y**2) / z)
-            
-                                                                   
+
 class OctaGeodesic(Geodesic):
     """
     Octahedron Geodesic Base
     ------------------------
     """
-    
-    
-    
-class IcosaGeodesic(Geodesic, GeodesicAngleMixin):
+    def phi(self, *args):
+        return super().phi(*args)
+
+    def theta(self, *args):
+        return super().theta(*args)
+
+
+class IcosaGeodesic(Geodesic):
     """
     Icosohedron (20-side) Geodesic Base
     -----------------------------------
     """
-    
-    ia = 2 * pi / 5
-    tau = (1 + sqrt(5)) / 2  # aka phi
-    
-    def _cvt(self, x: float, y: float, z: float, v: int):
-        """Bottom of page 74, transform req'd for icosa."""
-        x1 = x * math.sin(self.ia)   # Eq. 12.4
-        y1 = y + x * math.cos(ia)    # Eq. 12.5
-        z1 = v / 2 + z / self.tau    # Eq. 12.6
-        return (x1, y1, z1)
-       
-    def phi(self, x: float, y: float, z: float, v: int):
-        """Eq. 12.1 - p 74"""
-        x1, y1, z1 = self._cvt(x, y, z, v)
-        if y1 == 0:
-            return pi / 2
-        return atan(x1 / y1)
 
-    def theta(self, x: float, y: float, z: float, v: int):
-        """Eq 12.2 - p 74"""
-        x1, y1, z1 = self._cvt(x, y, z, v)
-        if z == 0:
-            return pi / 2
-        return atan(sqrt(x1**2 + y1**2) / z1)
-        
+    ia = 2 * pi / 5
+
+    def _cvt(self, x: float, y: float, z: float, v: int=1):
+        """Bottom of page 74, transform req'd for icosa."""
+        return (x * sin(self.ia),        # Eq. 12.4 - X1
+                y + x * cos(self.ia),    # Eq. 12.5 - Y1
+                v / 2 + z / tau)         # Eq. 12.6 - Z1
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,28 +1,32 @@
 
 from unittest import TestCase
-
 from ..base import *
 
-class TestFunctions(TestCase):
-    
+
+def checkEqualLists(a1, a2):
+    return len(a1) == len(a2) and sorted(a1) == sorted(a2)
+
+
+class TestGeodesic(TestCase):
+
     def test_rho(self):
-        
+
         g = Geodesic()
-        
+
         with self.assertRaises(TypeError):
             g.rho('a')
-            
+
         with self.assertRaises(TypeError):
             g.rho(1.4)
-            
+
         with self.assertRaises(ValueError):
             g.rho(0)
             
         with self.assertRaises(ValueError):
             g.rho(9)
             
-        rm = g.rho(1)
-        self.assertEqual(rm, [[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+        rm = sorted(g.rho(1))
+        self.assertEqual(rm, sorted([[0, 0, 1], [1, 0, 0], [0, 1, 0]]))
         
         rm = g.rho(2)
         self.assertEqual(rm,  [[0, 0, 0, 1, 1, 2], [2, 1, 0, 1, 0, 0], [0, 1, 2, 0, 1, 0]])
@@ -38,3 +42,44 @@ class TestFunctions(TestCase):
                               [0, 1, 2, 3, 4, 0, 1, 2, 3, 0, 1, 2, 0, 1, 0]])
         
         # This should satisfy us.
+        
+    def test_apices(self):
+        
+        g = Geodesic()
+        
+        with self.assertRaises(TypeError):
+            g.apices('a')
+            
+        with self.assertRaises(TypeError):
+            g.apices(2.3)
+            
+        with self.assertRaises(ValueError):
+            g.apices(0)
+            
+        am = g.apices(1)
+        self.assertTrue(checkEqualLists([a.xyz for a in am], [[0, 1, 0], [0, 0, 1], [1, 0, 0]]))
+
+        am = g.apices(2)
+        self.assertTrue(checkEqualLists([a.xyz for a in am], [[0, 2, 0], [0, 1, 1], [0, 0, 2], [1, 1, 0], [1, 0, 1], [2, 0, 0]]))
+        
+        am = g.apices(4)
+        self.assertTrue(checkEqualLists([a.xyz for a in am], [[0, 4, 0],
+                         [0, 3, 1],
+                         [0, 2, 2],
+                         [0, 1, 3],
+                         [0, 0, 4],
+                         [1, 3, 0],
+                         [1, 2, 1],
+                         [1, 1, 2],
+                         [1, 0, 3],
+                         [2, 2, 0],
+                         [2, 1, 1],
+                         [2, 0, 2],
+                         [3, 1, 0],
+                         [3, 0, 1],
+                         [4, 0, 0]]))
+        
+        def test_dist(self):
+            
+            g = Geodesic()
+            
